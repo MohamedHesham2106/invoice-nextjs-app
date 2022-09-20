d8d34b3bac626b85c1dfd261e67375cd926112eb
Mohamed Hesham <moohisham2000@gmail.com>
2022-09-20 21:22:16 +0200

    feat
    add new Invoice form

---
+++ pages/add-new/index.js
@@ -0,0 +1,10 @@
+import NewInvoice from "../../components/Invoice/NewInvoice/NewInvoice";
+const AddNew = () => {
+  return (
+    <section className="main_container">
+      <NewInvoice />
+    </section>
+  );
+};
+
+export default AddNew;
